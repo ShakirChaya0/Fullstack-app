0e6a2c81f6238b765a94fe9542ebd9209178bb6e
Shakir Chaya <Shakirchaya2005@gmail.com>
2025-05-24 02:04:13 -0300

    Agregar backend con código de desarrollo

---
+++ backend/routes/productsRoute.js
@@ -0,0 +1,50 @@
+import {Router} from 'express'
+import {ProductController} from '../controllers/productController.js'
+
+export function productosRouter () {
+    const productRouter = Router()
+
+    productRouter.get('/', (req, res) => {
+        const productos = ProductController.getAll()
+        res.json(productos)
+    })
+
+    productRouter.get('/id/:idProducto', (req, res) => {
+        const {idProducto} = req.params
+        const producto = ProductController.getById(idProducto)
+        res.json(producto)
+    })
+
+    productRouter.get('/nombre/:nombre', (req, res) => {
+        const {nombre} = req.params
+        const producto = ProductController.getByName(nombre)
+        res.json(producto)
+    })
+
+    productRouter.get('/tipo/:tipo', (req, res) => {
+        const {tipo} = req.params
+        const producto = ProductController.getByType(tipo)
+        res.json(producto)
+    })
+
+    productRouter.post('/crearProducto', (req, res) => {
+        const data = req.body
+        const producto = ProductController.create(data)
+        res.json(producto)
+    })
+
+    productRouter.patch('/actualizarProducto/:id', (req, res) => {
+        const data = req.body
+        const {id} = req.params
+        const producto = ProductController.update(data, id)
+        res.json(producto)
+    })
+
+    productRouter.delete('/eliminarProducto/:id', (req, res) => {
+        const {id} = req.params
+        const producto = ProductController.delete(id)
+        res.json(producto)
+    })
+
+    return productRouter
+}
