8102f0a6c214430194183e476a8f1eb785d22110
nicomazzag <nicolasmazzaglia.nm@gmail.com>
2025-08-22 22:45:38 -0300

    Trigger para manejar contador de Linea de Pedido por Pedido

--- backend/src/infrastructure/triggers/nroLinea.sql
+++ backend/src/infrastructure/triggers/nroLinea.sql
@@ -3,20 +3,19 @@
 CREATE OR REPLACE FUNCTION asignar_nro_linea()
 RETURNS TRIGGER AS $$
 BEGIN
-  -- Si nroLinea no se pasó manualmente
-
-  IF NEW.nroLinea IS NULL THEN
-    SELECT COALESCE(MAX(nroLinea), 0) + 1 -- Esa expresión calcula el siguiente número de línea disponible dentro de un pedido, empezando en 1 cuando no hay ninguna
-    INTO NEW.nroLinea
+  -- Si nroLinea es NULL o 0, calculamos el siguiente número
+  IF NEW."nroLinea" IS NULL OR NEW."nroLinea" = 0 THEN
+    SELECT COALESCE(MAX("nroLinea"), 0) + 1 -- Esta expresión calcula el siguiente número de línea disponible dentro de un pedido, empezando en 1 cuando no hay ninguna
+    INTO NEW."nroLinea"
     FROM "Linea De Pedido"
     WHERE "idPedido" = NEW."idPedido";
   END IF;
+
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
--- Trigger que ejecuta la función antes de insertar
-
+-- Trigger
 CREATE TRIGGER trigger_asignar_nro_linea
 BEFORE INSERT ON "Linea De Pedido"
 FOR EACH ROW
